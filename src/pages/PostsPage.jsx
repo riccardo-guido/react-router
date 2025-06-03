eb33db43f448a5d8f7793e65afe219bc26d9cc52
riccardo-guido <guidoriccardo02@gmail.com>
2025-06-03 16:58:01 +0200

    cors implemented and API call

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function PostsPage() {
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div>
       <div className="container">
